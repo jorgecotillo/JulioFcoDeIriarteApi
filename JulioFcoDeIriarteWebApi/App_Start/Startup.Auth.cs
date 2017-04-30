dc18e0d35ab210aefc0e16218989a8f775c8d333
Jorge Cotillo <jorge.cotillo@gmail.com>
2017-04-30 11:14:03 -0400

    Add project files.

---
+++ JulioFcoDeIriarteWebApi/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JulioFcoDeIriarteWebApi
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ JulioFcoDeIriarteWebApi/Controllers/ManageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JulioFcoDeIriarteWebApi.Controllers
+{
+    public class ManageController : ApiController
+    {
+        public string Get()
+        {
+            return "Ok";
+        }
+    }
+}
